392c3680d2bcbf12f8f4a85dd0c886a07d2b689e
Tomas Costantino <tomascostantino.au@gmail.com>
2025-12-09 10:24:14 +0100

    test board columns controller refreshes sibling columns

--- test/controllers/boards/columns_controller_test.rb
+++ test/controllers/boards/columns_controller_test.rb
@@ -19,6 +19,17 @@ class Boards::ColumnsControllerTest < ActionDispatch::IntegrationTest
     assert_equal "New Column", boards(:writebook).columns.last.name
   end
 
+  test "create refreshes sibling columns" do
+    board = boards(:writebook)
+    existing_columns = board.columns.to_a
+
+    post board_columns_path(board), params: { column: { name: "New Column" } }, as: :turbo_stream
+
+    existing_columns.each do |column|
+      assert_turbo_stream action: :replace, target: dom_id(column)
+    end
+  end
+
   test "update" do
     column = columns(:writebook_in_progress)
 
@@ -34,6 +45,18 @@ class Boards::ColumnsControllerTest < ActionDispatch::IntegrationTest
     assert_difference -> { boards(:writebook).columns.count }, -1 do
       delete board_column_path(boards(:writebook), column), as: :turbo_stream
       assert_response :success
+    end
+  end
+
+  test "destroy refreshes sibling columns" do
+    board = boards(:writebook)
+    column = columns(:writebook_on_hold)
+    sibling_columns = column.sibling_columns.to_a
+
+    delete board_column_path(board, column), as: :turbo_stream
+
+    sibling_columns.each do |sibling_column|
+      assert_turbo_stream action: :replace, target: dom_id(sibling_column)
     end
   end
 end
